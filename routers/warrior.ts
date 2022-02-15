9b2a72e4a6c1a4700f2e76875eae4494547411ba
id github: wizabela <izabela.wyrebska@gmail.com>
2022-02-15 19:48:28 +0100

    convert to Number some stats from req.body

--- routers/warrior.ts
+++ routers/warrior.ts
@@ -1,4 +1,5 @@
 import {Router} from "express";
+import { WarriorRecord } from "../records/warrior.record";
 
 export const warriorRouter = Router();
 
@@ -8,5 +9,12 @@ warriorRouter
        res.render('warrior/add-form');
     })
     .post('/', (req, res) => {
+        const warrior = new WarriorRecord({
+            ...req.body,
+            power: Number(req.body.power),
+            defence: Number(req.body.defence),
+            resilience: Number(req.body.resilience),
+            agility: Number(req.body.agility),
+        });
         res.render('warrior/warrior-added');
     });
