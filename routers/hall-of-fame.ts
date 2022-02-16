84c8e7bd17de44f4f26462bc08d1473701920a6c
id github: wizabela <izabela.wyrebska@gmail.com>
2022-02-16 16:20:44 +0100

    change place for starting from 1

--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -6,7 +6,12 @@ export const hallOfFameRouter = Router();
 hallOfFameRouter
 
     .get('/', async (req, res) => {
-        const warriors = await WarriorRecord.listTop(10);
+        const warriors = (await WarriorRecord.listTop(10)).map((warrior, index) => {
+            return {
+                place: index + 1,
+                warrior,
+            };
+        });
        res.render('hall-of-fame/list', {
            warriors,
        });
