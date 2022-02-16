8ff8e278eae4a94dd43dd721f5b27a12778811b5
id github: wizabela <izabela.wyrebska@gmail.com>
2022-02-16 16:25:21 +0100

    finished hall of fame

--- routers/hall-of-fame.ts
+++ routers/hall-of-fame.ts
@@ -6,12 +6,14 @@ export const hallOfFameRouter = Router();
 hallOfFameRouter
 
     .get('/', async (req, res) => {
-        const warriors = (await WarriorRecord.listTop(10)).map((warrior, index) => {
+        const warriors = (await WarriorRecord.listTop(10) //pobieramy 10topowych warriors
+        ).map((warrior, index) => { //i zmieniamy je mapą tak, by dostać obiekt, w którym oprócz warrior mamy place zwiększone o 1
             return {
                 place: index + 1,
                 warrior,
             };
         });
+
        res.render('hall-of-fame/list', {
            warriors,
        });
