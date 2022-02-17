23134840a0ade80c48e0d7bdf07d6541fbc6bfff
id github: wizabela <izabela.wyrebska@gmail.com>
2022-02-17 16:21:21 +0100

    create error message for selecting warriors

--- routers/arena.ts
+++ routers/arena.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {WarriorRecord} from "../records/warrior.record";
+import {ValidationError} from "../utlis/errors";
 
 export const arenaRouter = Router();
 
@@ -7,10 +8,26 @@ arenaRouter
 
     .get('/fight-form', async (req, res) => {
         const warriors = await WarriorRecord.listAll();
-       res.render('arena/fight-form', {
-           warriors,
-       });
+        res.render('arena/fight-form', {
+            warriors,
+        });
     })
-    .post('/fight', (req, res) => {
+    .post('/fight', async (req, res) => {
+        const {warrior1: warrior1Id, warrior2: warrior2Id} = req.body;
+
+        if (warrior1Id === warrior2Id) {
+            throw new ValidationError(`Proszę wybrać dwóch różnych przeciwników.`);
+        }
+        const warrior1 = await WarriorRecord.getOne(warrior1Id);
+        const warrior2 = await WarriorRecord.getOne(warrior2Id);
+
+        if(!warrior1) {
+            throw new ValidationError(`Nie znaleziono przeciwnika nr 1.`);
+        }
+        if(!warrior2) {
+            throw new ValidationError(`Nie znaleziono przeciwnika nr 2.`);
+        }
+
+
         res.render('arena/fight');
     }); //POST /arena/fight
