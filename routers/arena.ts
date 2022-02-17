8f327ace91c42656af602013d8e1350b6a0a8fe2
id github: wizabela <izabela.wyrebska@gmail.com>
2022-02-17 15:48:45 +0100

    add partials hbs for selecting warrior

--- routers/arena.ts
+++ routers/arena.ts
@@ -1,11 +1,15 @@
 import {Router} from "express";
+import {WarriorRecord} from "../records/warrior.record";
 
 export const arenaRouter = Router();
 
 arenaRouter
 
-    .get('/fight-form', (req, res) => {
-       res.render('arena/fight-form');
+    .get('/fight-form', async (req, res) => {
+        const warriors = await WarriorRecord.listAll();
+       res.render('arena/fight-form', {
+           warriors,
+       });
     })
     .post('/fight', (req, res) => {
         res.render('arena/fight');
